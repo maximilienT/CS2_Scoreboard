f09d096ef646865b430d76746675e3c6068c27c9
Max <maxthacker1@gmail.com>
2026-08-06 16:03:22 -0700

    Finished with assignment

--- CS2_scoreboard.py
+++ CS2_scoreboard.py
@@ -3,13 +3,13 @@ import pandas as pd
 # Load in CSV file as a dataframe
 df = pd.read_csv("example_player_death.csv")
 
-# Group by attacker_id and gets number of rows as kills
+# Group by attacker_id and gets number of rows as kills as each attacker event is a kill
 kills = df.groupby('attacker_id_fixed').size().rename("kills")
 
-# Group by player_id and gets number of rows as deaths
+# Group by player_id and gets number of rows as deaths as each player_id event in this context is a death
 deaths = df.groupby('player_id_fixed').size().rename("deaths")
 
-# Concat kills and deaths together to get scoreboard
+# Concat kills and deaths together to get scoreboard. fillna(0) handles players with either no kill or deaths
 scoreboard = pd.concat([kills, deaths], axis=1).fillna(0).astype(int)
 
 # Gets index as its own column, name it player_id_fixed, and cast to int (source column is float due to NaN rows)
