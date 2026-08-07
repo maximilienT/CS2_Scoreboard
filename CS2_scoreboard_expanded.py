19ec522869f5478b8ffa6958139021c375a3c661
Max <maxthacker1@gmail.com>
2026-08-06 18:35:03 -0700

    Added more metrics (assists, k/d, best weapon, longest kill dist)

--- CS2_scoreboard_expanded.py
+++ CS2_scoreboard_expanded.py
@@ -18,12 +18,13 @@ df['kill_dist'] = df.apply(
 # Group by attacker_id and gets number of rows as kills as each attacker event is a kill
 kills = df.groupby('attacker_id_fixed').size().rename("kills")
 
-# favorite_weapon = df.groupby()
+# Group by attacker_id and find the most common weapon used for kills
+best_weapon = df.groupby('attacker_id_fixed')['weapon_name'].agg(lambda x: x.mode().iat[0]).rename('best weapon')
 
 # Group by player_id and gets number of rows as deaths as each player_id event in this context is a death
 deaths = df.groupby('player_id_fixed').size().rename("deaths")
 
-# Groupp by assister_id_fixed and gets number of rows as assists as each assister_id even is an assist.
+# Group by assister_id_fixed and gets number of rows as assists as each assister_id even is an assist.
 assists = df.groupby('assister_id_fixed').size().rename("assists")
 
 # Group by attacker_id and get their largest kill distance
@@ -31,6 +32,7 @@ longest_kill_dist = df.groupby('attacker_id_fixed')['kill_dist'].max().rename("l
 
 # Concat kills and deaths together to get scoreboard. fillna(0) handles players with either no kill or deaths
 scoreboard = pd.concat([kills, deaths, assists, longest_kill_dist], axis=1).fillna(0).astype(int)
+scoreboard['best weapon'] = best_weapon
 
 # Gets index as its own column, name it player_id_fixed, and cast to int (source column is float due to NaN rows)
 scoreboard = scoreboard.reset_index().rename(columns={'index':'player_id_fixed'})
@@ -43,8 +45,9 @@ scoreboard['K/D Ratio'] = round(scoreboard['kills'] / scoreboard['deaths'],2)
 scoreboard = scoreboard.sort_values(by=['kills', 'deaths', 'assists', 'K/D Ratio','longest kill distance','player_id_fixed'], ascending=[False, False, False, False, False, False])
 
 # Final organization of scoreboard
-scoreboard = scoreboard[['player_id_fixed','kills','deaths','assists','K/D Ratio','longest kill distance']]
+scoreboard = scoreboard[['player_id_fixed','kills','deaths','assists','K/D Ratio','best weapon', 'longest kill distance']]
 
-print(scoreboard)
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+    print(scoreboard)
 # Export the scoreboard to csv
 # scoreboard.to_csv("scoreboard.csv", index=False)
